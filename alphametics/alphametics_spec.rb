df76e5113001a085199e6a4f3f3b1b98465ca9d9
Nikki Sharifli <nigar.sherifli@gmail.com>
2017-05-17 17:06:26 -0400

    Refactores spec file

--- alphametics/alphametics_spec.rb
+++ alphametics/alphametics_spec.rb
@@ -2,33 +2,34 @@ require "rspec"
 require "./alphametics"
 
 describe Alphametics do
+  describe ".solve" do
+    xit "solves for two-character expression" do
+      expression = "A + A = B"
 
-  xit "solves for two-character expression" do
-    expression = "A + A = B"
+      result = Alphametics.solve(expression)
 
-    result = Alphametics.solve(expression)
+      expect(result).to eq({
+        A: 1,
+        B: 2
+      })
+    end
 
-    expect(result).to eq({
-      A: 1,
-      B: 2
-    })
-  end
+    xit "replaces characters with valid numbers" do
+      expression = "SEND + MORE = MONEY"
 
-  xit "replaces characters with valid numbers" do
-    expression = "SEND + MORE = MONEY"
+      result = Alphametics.solve(expression)
 
-    result = Alphametics.solve(expression)
-
-    expect(result).to eq({
-      E: 5,
-      S: 9,
-      N: 6,
-      D: 7,
-      M: 1,
-      O: 0,
-      R: 8,
-      Y: 2
-    })
+      expect(result).to eq({
+        E: 5,
+        S: 9,
+        N: 6,
+        D: 7,
+        M: 1,
+        O: 0,
+        R: 8,
+        Y: 2
+      })
+    end
   end
 
   describe ".split" do
