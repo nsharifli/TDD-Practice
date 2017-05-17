29eec526576dcef45cb04a35e3704902ce483b4f
Nikki Sharifli <nigar.sherifli@gmail.com>
2017-05-17 11:51:42 -0400

    First test passed

--- alphametics/alphametics_spec.rb
+++ alphametics/alphametics_spec.rb
@@ -3,8 +3,20 @@ require './alphametics'
 
 describe Alphametics do
 
-  it 'does something' do
+  it 'replaces characters with valid numbers' do
+    input = "SEND + MORE = MONEY"
 
-    expect(true).to eq(false)
+    result = Alphametics.solve(input)
+
+    expect(result).to eq({
+      E: 5,
+      S: 9,
+      N: 6,
+      D: 7,
+      M: 1,
+      O: 0,
+      R: 8,
+      Y: 2
+    })
   end
 end
